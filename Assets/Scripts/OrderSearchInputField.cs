150085f1fb29935286f551424aee52fbce3d0dca
steve97harris <steve97harris@hotmail.co.uk>
2020-08-01 16:04:59 +0100

    lkjd

---
+++ Assets/Scripts/OrderSearchInputField.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class OrderSearchInputField : MonoBehaviour
+    {
+        public void OnInputValueChanged(string customerName)
+        {
+            var orderEntryObjects = GameObjectFinder.FindObjectsByName("OrderEntry(Clone)");
+
+            for (int i = 0; i < orderEntryObjects.Length; i++)
+            {
+                var entryName = orderEntryObjects[i].GetComponent<OrderEntryUi>().entryCustomerName.text;
+
+                if (!entryName.Contains(customerName))
+                    orderEntryObjects[i].SetActive(false);
+            }
+        }
+
+        public void OnDeselect(string customerName)
+        {
+            var orderEntryObjects = GameObjectFinder.FindObjectsByName("OrderEntry(Clone)");
+
+            if (customerName != "") 
+                return;
+            
+            for (int i = 0; i < orderEntryObjects.Length; i++)
+            {
+                orderEntryObjects[i].SetActive(true);
+            }
+        }
+    }
+}
